31a8f23079f997f32a32d58de339c2da46acd741
Ryan <Ryan@10.18.149.20>
2013-10-18 12:04:16 -0600

    added player.java

---
+++ src/connectFour/Player.java
@@ -0,0 +1,68 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package connectFour;
+
+/**
+ *
+ * @author Ryan
+ */
+public class Player {
+    
+    public static final String REGULAR_PLAYER = "REGULAR";
+    public static final String COMPUTER_PLAYER = "COMPUTER"; 
+
+    private String name;
+    private String playerType;
+    private String marker;
+    
+
+
+
+    public Player(){
+    }
+    
+    public Player(String playerType, String marker) {
+        this.marker = marker;
+        this.playerType = playerType;
+    }
+    
+    public String getName() {
+        return name;
+    }
+    
+    public void setName(String name) {
+        this.name = name;
+    }
+    
+    public String getPlayerType() {
+        return playerType;
+    }
+    
+    public void setPlayerType(String playerType) {
+        this.playerType = playerType;
+    }
+    
+    public String getMarker() {
+        return marker;
+    }
+    
+    public void setMarker(String marker) {
+        this.marker = marker;
+    }
+        
+}
+
+
+
+
+
+
+
+
+
+
+
+
+

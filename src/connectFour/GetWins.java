9077f9399a63ceb7e12c67810d01775a53f0eb9f
Noble <Noble@Nobles-Laptop.mshome.net>
2013-10-30 12:31:27 -0600

    This function is in charge of getting the wins of the Users and sorting them from Greatest to Least.

---
+++ src/connectFour/GetWins.java
@@ -0,0 +1,32 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package connectFour;
+
+import java.util.Scanner;
+/**
+ *
+ * @author Noble
+ */
+public class GetWins {
+    
+    public static int [] sortScores ( int [ ] num )
+{ 
+     int i, j, temp;  //be sure that the temp variable is the same "type" as the array
+     for ( i = 0; i < num.length - 1; i ++ )  
+     {
+          for ( j = i + 1; j < num.length; j ++ )
+          {
+               if( num[ i ] < num[ j ] )         //sorting into descending order
+               {
+                       temp = num[ i ];   //swapping
+                       num[ i ] = num[ j ];
+                       num[ j ] = temp; 
+                }           
+          }
+     }
+     return num;
+}
+}
+        

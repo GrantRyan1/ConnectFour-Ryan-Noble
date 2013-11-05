a437b376d4a170bedf68bd728c89a627b829e234
Ryan <Ryan@10.18.149.20>
2013-11-05 12:28:45 -0700

    Cleared errors with Bro. J 2

--- src/connectFour/BoardView.java
+++ src/connectFour/BoardView.java
@@ -10,77 +10,65 @@ package connectFour;
  */
 public class BoardView {
  
-    Board board;
-    Board board;
+    Board board;    
     
-    public void displayBoard(Board board) {
-        this.board = board;
-        
-        this.printHeadRow(); 
-        this.printDividerRow(); 
-        
-    
+    public Object displayBoard(Object object) {
+        this.board = (Board) object;
+        this.printHeadRow();
+        this.printDividerRow();
         for (int i = 0; i < this.board.getRowCount(); i++) {
-           
-            Player[] rowOfLocations = this.board.getMarkerLocations()[i];
-            
-            
-            this.printRow(i+1, rowOfLocations); 
-            
+            Player[] rowOfLocations = this.board.getBoardLocations()[i];
+            this.printRow(i+1, rowOfLocations);
             this.printDividerRow();
         }
-        
-        System.out.println(); 
+        System.out.println();
          
-        return;
+        return null;
     }
-
+    
     private void printHeadRow() {
 
-        // print the first column in the header row
+        // print first cell
+
         System.out.print("\n\t      1   ");
-        
-        // print remaining header cells in row between the first and last column
         int columnsInRow = this.board.getColumnCount();
+        // print remaining cells in row
         for (int i = 1; i < columnsInRow - 1; i++) {
             int col = i + 1;
             System.out.print("  " + col + "   ");
         }
-        // print the header for the last column in the header
         System.out.print(" " + columnsInRow + "   ");
     }
 
     private void printDividerRow() {
 
-        // print the divider for the first column in the row
+        // print first cell
+
         System.out.print("\n\t  |------");
-        
         int columnsInRow = this.board.getColumnCount();
-        // print remaining divider for each column between the first and last
+        // print remaining cells in row
         for (int i = 1; i < columnsInRow - 1; i++) {
             System.out.print("------");
         }
-        // print the row divider for the last column in the row
         System.out.print("-----|");
     }
 
     private void printRow(int rowNumber, Player[] rowLocations) {
         
-        // print contents of first column in the row
+        // print first cell
         String letter = " ";
         if (rowLocations[0] != null) {
-            letter = rowLocations[0].getMarker();
+            letter = rowLocations[0].marker;
         }
         System.out.print("\n\t" + rowNumber + " |  " + letter + "  |");
 
-        // print the contents of the rest of the columns in the row 
+        // print remaining cells in row
         for (int i = 1; i < rowLocations.length; i++) {
             if (rowLocations[i] == null) {
                 letter = " ";
             } else {
-                letter = rowLocations[i].getMarker();
+                letter = rowLocations[i].marker;
             }
-            
             System.out.print("  " + letter + "  |");
         }
     }

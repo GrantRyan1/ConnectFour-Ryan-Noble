36cd8d4f93b1ade32dbdf9ef78603e60613ff75a
Noble <Noble@Nobles-Laptop.mshome.net>
2013-11-07 15:51:13 -0600

    Trying to get everything ready to push up for the presentation at the end of the week.

--- src/connectFour/Game.java
+++ src/connectFour/Game.java
@@ -9,15 +9,17 @@ package connectFour;
  * @author Ryan
  */
 class Game {
-    static String PLAYING;
-    static Object QUIT;
-    static Object ONE_PLAYER;
-    static Object TWO_PLAYER;
-    static String EXIT;
-    static Object Playing;
-    String PLAYER_A_DEFAULT_MARKER;
-    String PLAYER_B_DEFAULT_MARKER;
-    Object setStatus;
+    public static String PLAYING;
+    public static Object QUIT;
+    public static Object ONE_PLAYER;
+    public static Object TWO_PLAYER;
+    public static String EXIT;
+    public static Object Playing;
+    public String PLAYER_A_DEFAULT_MARKER;
+    public String PLAYER_B_DEFAULT_MARKER;
+    public Object setStatus;
+    public Player PlayerA;
+    public Player PlayerB;
 
     Game(Object ONE_PLAYER) {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
@@ -27,11 +29,11 @@ class Game {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
-    void setPlayerA(Player playerA) {
+    void PlayerA(Player playerA) {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
-    void setPlayerB(Player playerB) {
+    void PlayerB(Player playerB) {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
